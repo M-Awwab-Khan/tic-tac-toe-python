062cb9fc950cfce8393d9c21b06bcd34c90e7c31
Awwab <mawwabkhank2006@gmail.com>
2024-02-24 20:31:16 +0500

    make move and print board functions

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ class TicTacToe:
         self.board = [['   ', ' | ', '   ', ' | ', '   '], ['   ', ' | ', '   ', ' | ', '   '], ['   ', ' | ', '   ', ' | ', '   ']]
         self.borders = ['---------------', '---------------', '']
 
-        for i in range(3):
-            for j in range(5):
-                print(self.board[i][j], end='')
-            print()
-            print(self.borders[i])
+        self.print_board()
         self.make_move()
 
     def make_move(self):
         pos = input('Enter position of X in r,c form: ')
         r, c = map(int, pos.split(','))
-        print(r, c)
+        self.board[r-1][2*c - 2] = ' X '
+        self.print_board()
+
+    def print_board(self):
+        for i in range(3):
+            for j in range(5):
+                print(self.board[i][j], end='')
+            print()
+            print(self.borders[i])
 
 if __name__ == '__main__':
     game = TicTacToe()
