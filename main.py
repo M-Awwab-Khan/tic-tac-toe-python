8c4b77edfcfe755cb2bd9644138fa3712b2b1b0f
Awwab <mawwabkhank2006@gmail.com>
2024-02-24 20:25:56 +0500

    class based setup

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-board = [['   ', ' | ', '   ', ' | ', '   '], ['   ', ' | ', '   ', ' | ', '   '], ['   ', ' | ', '   ', ' | ', '   ']]
-borders = ['---------------', '---------------', '']
 
-for i in range(3):
-    for j in range(5):
-        print(board[i][j], end='')
-    print()
-    print(borders[i])
+class TicTacToe:
+    def __init__(self) -> None:
+        self.board = [['   ', ' | ', '   ', ' | ', '   '], ['   ', ' | ', '   ', ' | ', '   '], ['   ', ' | ', '   ', ' | ', '   ']]
+        self.borders = ['---------------', '---------------', '']
+
+        for i in range(3):
+            for j in range(5):
+                print(self.board[i][j], end='')
+            print()
+            print(self.borders[i])
+        self.make_move()
+
+    def make_move(self):
+        pos = input('Enter position of X in r,c form: ')
+        r, c = map(int, pos.split(','))
+        print(r, c)
+
+if __name__ == '__main__':
+    game = TicTacToe()
