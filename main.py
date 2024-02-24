fcc7adf012fc33a3c9aa46132dfe40cca86f6819
Awwab <mawwabkhank2006@gmail.com>
2024-02-24 20:43:07 +0500

    mark already present validation

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ class TicTacToe:
         self.borders = ['---------------', '---------------', '']
         self.current_player = 'X'
         while True:
-            self.current_player = 'O' if self.current_player == 'X' else 'X'
             self.print_board()
             self.make_move(self.current_player)
 
     def make_move(self, char):
         pos = input('Enter position of X in r,c form: ')
         r, c = map(int, pos.split(','))
-        self.board[r-1][2*c - 2] = f' {char} '
+        if self.board[r-1][2*c - 2] == '   ':
+            self.board[r-1][2*c - 2] = f' {char} '
+            self.current_player = 'O' if self.current_player == 'X' else 'X'
+        else:
+            print('Position Already Marked!')
 
     def print_board(self):
         for i in range(3):
