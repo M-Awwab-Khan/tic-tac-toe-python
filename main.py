de58901509a0cacb883df89e0374b45dd6812ac6
Awwab <mawwabkhank2006@gmail.com>
2024-02-24 21:25:20 +0500

    check winner algorithm

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ class TicTacToe:
             self.board[r-1][2*c - 2] = f' {char} '
             self.player_positions[char][r-1, c-1] = 1
             print(self.player_positions[char]) # for debugging
+            print(self.check_winner(self.current_player))
             self.current_player = 'O' if self.current_player == 'X' else 'X'
         else:
             print('Position Already Marked!')
@@ -34,6 +35,21 @@ class TicTacToe:
                 print(self.board[i][j], end='')
             print()
             print(self.borders[i])
+    
+    def check_winner(self, char):
+        winner = None
+        for row_sum in self.player_positions[char].sum(axis=1):
+            if row_sum == 3:
+                winner = char
+        for col_sum in self.player_positions[char].sum(axis=0):
+            if col_sum == 3:
+                winner = char
+        if np.trace(self.player_positions[char]) == 3:
+            winner = char
+        elif np.trace(np.fliplr(self.player_positions[char])) == 3:
+            winner = char
+        return winner
+
 
 if __name__ == '__main__':
     game = TicTacToe()
